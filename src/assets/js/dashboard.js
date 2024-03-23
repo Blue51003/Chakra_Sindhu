8bd735a6a94203689aa3ad875f84ab6e6c5b6331
abhisiddanthi <123393611+abhisiddanthi@users.noreply.github.com>
2024-03-23 16:07:27 +0530

    data update

--- src/assets/js/dashboard.js
+++ src/assets/js/dashboard.js
@@ -1,297 +1,137 @@
+function fetchJSONData() {
+  return fetch('../../src/data/data.json')
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => {
+          return data;
+      })
+      .catch(error => {
+          console.error('Error reading file:', error);
+          throw error;
+      });
+}
+
 $(function () {
-
-  // const fs = require('fs');
-  // const csvFilePath = 'your_dynamic_data.csv';
-
-  // let series = [];
-
-  // function csvToJson() {
-  //   const jsonArray = [];
-
-  //   // Read CSV file
-  //   const csvData = fs.readFileSync(csvFilePath, 'utf8');
-  //   const rows = csvData.split('\n');
-
-  //   // Extract column headers
-  //   const headers = rows[0].split(',');
-
-  //   // Convert each row to a JSON object
-  //   for (let i = 1; i < rows.length; i++) {
-  //     const values = rows[i].split(',');
-  //     const obj = {};
-  //     for (let j = 0; j < headers.length; j++) {
-  //       obj[headers[j].trim()] = values[j].trim();
-  //     }
-  //     jsonArray.push(obj);
-  //   }
-
-  //   // Update the 'series' variable
-  //   series = jsonArray;
-
-  //   console.log('CSV data converted to series:', series);
-  // }
-
-  // Run the conversion initially
-  // csvToJson();
-
-  // Update every 1 minute (60000 milliseconds)
-  // setInterval(csvToJson, 60000);
-
-
+  fetchJSONData()
+  .then(series => {
+  
   // =====================================
-  // Profit
+  // Time Series Wind Speed Main
   // =====================================
-  var chart = {
-    series: [
-      { name: "Earnings this month:", data: [355, 390, 300, 350, 390, 180, 355, 390] },
-      { name: "Expense this month:", data: [280, 250, 325, 215, 250, 310, 280, 250] },
-    ],
 
+  var wind_speed_main = {
+    series: [{
+    data: series.wind_speed
+  }],
     chart: {
-      type: "bar",
-      height: 345,
-      offsetX: -15,
-      toolbar: { show: true },
-      foreColor: "#adb0bb",
-      fontFamily: 'inherit',
-      sparkline: { enabled: false },
-    },
-
-
-    colors: ["#5D87FF", "#49BEFF"],
-
-
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: "35%",
-        borderRadius: [6],
-        borderRadiusApplication: 'end',
-        borderRadiusWhenStacked: 'all'
-      },
-    },
-    markers: { size: 0 },
-
-    dataLabels: {
-      enabled: false,
-    },
-
-
-    legend: {
-      show: false,
-    },
-
-
-    grid: {
-      borderColor: "rgba(0,0,0,0.1)",
-      strokeDashArray: 3,
-      xaxis: {
-        lines: {
-          show: false,
-        },
-      },
-    },
-
-    xaxis: {
-      type: "category",
-      categories: ["16/08", "17/08", "18/08", "19/08", "20/08", "21/08", "22/08", "23/08"],
-      labels: {
-        style: { cssClass: "grey--text lighten-2--text fill-color" },
-      },
-    },
-
-
-    yaxis: {
-      show: true,
-      min: 0,
-      max: 400,
-      tickAmount: 4,
-      labels: {
+    height: 350,
+    type: 'line',
+    id: 'areachart-2'
+  },
+  annotations: {
+    yaxis: [{
+      y: 1.3,
+      borderColor: '#00E396',
+      label: {
+        borderColor: '#00E396',
         style: {
-          cssClass: "grey--text lighten-2--text fill-color",
+          color: '#fff',
+          background: '#00E396',
         },
-      },
-    },
-    stroke: {
-      show: true,
-      width: 3,
-      lineCap: "butt",
-      colors: ["transparent"],
-    },
-
-
-    tooltip: { theme: "light" },
-
-    responsive: [
-      {
-        breakpoint: 600,
-        options: {
-          plotOptions: {
-            bar: {
-              borderRadius: 3,
-            }
-          },
-        }
+        text: 'Cut-in',
       }
-    ]
-
-
+    },
+    {
+      y: 4.2,
+      borderColor: 'blue',
+      label: {
+        borderColor: 'black',
+        style: {
+          color: '#fff',
+          background: 'blue',
+        },
+        text: 'Rated',
+      }
+    },
+    {
+      y: 9.0,
+      borderColor: 'red',
+      label: {
+        borderColor: 'black',
+        style: {
+          color: '#000',
+          background: 'red',
+        },
+        text: 'Cut-out',
+      }
+    }],
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  grid: {
+    padding: {
+      right: 30,
+      left: 20
+    }
+  },
+  labels: series.time,
+  xaxis: {
+    type: 'datetime',
+  },
   };
 
-  var chart = new ApexCharts(document.querySelector("#chart"), chart);
+  var chart = new ApexCharts(document.querySelector("#wind_speed_main"), wind_speed_main);
   chart.render();
 
+  // =====================================
+  // Time Series Wind Direction Main
+  // =====================================
 
-  // =====================================
-  // Breakup
-  // =====================================
-  var breakup = {
-    color: "#adb5bd",
-    series: [38, 40, 25],
-    labels: ["2022", "2021", "2020"],
+  var wind_dir_main = {
+    series: [{
+    data: series.wind_direction
+  }],
     chart: {
-      width: 180,
-      type: "donut",
-      fontFamily: "Plus Jakarta Sans', sans-serif",
-      foreColor: "#adb0bb",
-    },
-    plotOptions: {
-      pie: {
-        startAngle: 0,
-        endAngle: 360,
-        donut: {
-          size: '75%',
-        },
-      },
-    },
-    stroke: {
-      show: false,
-    },
-
-    dataLabels: {
-      enabled: false,
-    },
-
-    legend: {
-      show: false,
-    },
-    colors: ["#5D87FF", "#ecf2ff", "#F9F9FD"],
-
-    responsive: [
-      {
-        breakpoint: 991,
-        options: {
-          chart: {
-            width: 150,
-          },
-        },
-      },
-    ],
-    tooltip: {
-      theme: "dark",
-      fillSeriesColor: false,
-    },
+    height: 350,
+    type: 'line',
+    id: 'areachart-2'
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  grid: {
+    padding: {
+      right: 30,
+      left: 20
+    }
+  },
+  labels: series.time,
+  xaxis: {
+    type: 'datetime',
+  },
   };
 
-  var chart = new ApexCharts(document.querySelector("#breakup"), breakup);
+  var chart = new ApexCharts(document.querySelector("#wind_dir_main"), wind_dir_main);
   chart.render();
+      // Now you can use the 'series' variable in this function
+      console.log(series); // Just to verify if the data is loaded correctly
+      // Call any other function or perform operations with 'series' here
+  })
+  .catch(error => {
+      // Handle errors here
+      console.error('Error:', error);
+  });
 
-
-
-  // =====================================
-  // Earning
-  // =====================================
-  var earning = {
-    chart: {
-      id: "sparkline3",
-      type: "line",
-      height: 300,
-      color: 'black',
-      // sparkline: {
-      //   enabled: true,
-      // },
-      group: "sparklines",
-      fontFamily: "Plus Jakarta Sans', sans-serif",
-      foreColor: "#adb0bb",
-    },
-    series: [
-      {
-        name: "Earnings",
-        color: "black",
-        data: [25, 66, 20, 40, 12, 58, 20],
-      },
-    ],
-    stroke: {
-      curve: "smooth",
-      width: 2,
-    },
-    fill: {
-      colors: ["#ffffff"],
-      type: "solid",
-    },
-
-    markers: {
-      size: 0,
-    },
-    tooltip: {
-      theme: "dark",
-      fixed: {
-        enabled: true,
-        position: "right",
-      },
-      x: {
-        show: true,
-      },
-    },
-  };
-  new ApexCharts(document.querySelector("#earning"), earning).render();
-
-  // =====================================
-  // Earning2
-  // =====================================
-  var earning2 = {
-    chart: {
-      id: "sparkline3",
-      type: "line",
-      height: 300,
-      color: 'black',
-      // sparkline: {
-      //   enabled: true,
-      // },
-      group: "sparklines",
-      fontFamily: "Plus Jakarta Sans', sans-serif",
-      foreColor: "#adb0bb",
-    },
-    series: [
-      {
-        name: "Earnings",
-        color: "black",
-        data: [25, 66, 20, 40, 12, 58, 20],
-      },
-    ],
-    stroke: {
-      curve: "smooth",
-      width: 2,
-    },
-    fill: {
-      colors: ["#ffffff"],
-      type: "solid",
-    },
-
-    markers: {
-      size: 0,
-    },
-    tooltip: {
-      theme: "dark",
-      fixed: {
-        enabled: true,
-        position: "right",
-      },
-      x: {
-        show: true,
-      },
-    },
-  };
-  new ApexCharts(document.querySelector("#earning2"), earning2).render();
 })
 
